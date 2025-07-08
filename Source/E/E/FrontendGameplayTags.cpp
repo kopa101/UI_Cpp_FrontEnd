f618fc4cafc2ddcc815e6464f191acd6f64ea178
anhyewon <hyewon.daniel@gmail.com>
2025-07-08 10:44:48 +0900

    added frame

---
+++ Source/E/E/FrontendGameplayTags.cpp
@@ -0,0 +1,13 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "FrontendGameplayTags.h"
+
+
+FrontendGameplayTags::FrontendGameplayTags()
+{
+}
+
+FrontendGameplayTags::~FrontendGameplayTags()
+{
+}
